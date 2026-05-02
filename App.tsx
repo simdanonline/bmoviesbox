fe40fc88b7b28979fc448cfd13ab11021fad7e47
similoluwa <similoluwa.odeyemi@gmail.com>
2026-05-03 00:57:32 +0100

    refactor: Remove unused imports and clean up App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AppStack from "./src/navigation/AppStack";
-import * as Updates from "expo-updates";
-import * as Device from "expo-device";
-import { Alert } from "react-native";
-
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
